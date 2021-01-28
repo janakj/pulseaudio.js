0013b00a654900f5a59101e6abc697d021fe6654
Jan Janak <jan@janakj.org>
2021-01-28 16:59:17 -0500

    Export more symbols to the aplication

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,8 @@ export {
     PA_DEFAULT_SOURCE,
     PA_DEFAULT_SINK,
     PA_NO_INDEX,
+    PA_NO_VALUE,
+    PA_MAX_CHANNELS,
     PA_NATIVE_COOKIE_LENGTH,
     PA_SINK_FLAGS,
     PA_SOURCE_FLAGS,
@@ -31,5 +33,7 @@ export {
     PlaybackStream,
     UploadStream,
     RecordStream,
-    sampleSize
+    sampleSize,
+    frameSize,
+    sampleFormatStr
 } from './stream';
