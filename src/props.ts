bc642a00b56847ee3ee670a436e3d55854951deb
Jan Janak <jan@janakj.org>
2024-04-06 21:04:44 -0400

    Improve property conversion functions
    The inflate and deflate functions should be capable of handling
    properties that have a primitive value and sub-properties at the same
    time. When such a property is discovered, we store the property's
    primitive value under its name prefixed with @ to make space for
    sub-properties in the object tree. Both inflate and deflate this
    conversion.

--- src/props.ts
+++ src/props.ts
@@ -24,62 +24,87 @@ export interface Props {
 
 // Convert a tree of properties such as
 // {
-//   application : {
-//     name : 'User Terminal',
-//     id : 'user-terminal',
-//     version : '1.0',
-//     process : {
-//       id : '40204',
-//       binary : 'ut',
-//       user : 'root',
-//       host : 'ut3',
-//       machine_id : '13123214124'
+//     "application" : {
+//         "name"     : "User Terminal",
+//         "id"       : "user-terminal",
+//         "version"  : "1.0",
+//         "@process" : "name",
+//         "process"  : {
+//             "id"         : "40204",
+//             "binary"     : "ut",
+//             "user"       : "root",
+//             "host"       : "ut3",
+//             "machine_id" : "13123214124"
+//         }
 //     }
-//   }
 // }
 //
-// into a flat object such as
+// into a flat object of key-value pairs such as
+//
 // {
-//  'application.name': 'User Terminal',
-//  'application.id': 'user-terminal',
-//  'application.version': '1.0',
-//  'application.process.id': '40204',
-//  'application.process.binary': 'ut',
-//  'application.process.user': 'root',
-//  'application.process.host': 'ut3',
-//  'application.process.machine_id': '13123214124'
+//     "application.name"               : "User Terminal",
+//     "application.id"                 : "user-terminal",
+//     "application.version"            : "1.0",
+//     "application.process"            : "name",
+//     "application.process.id"         : "40204",
+//     "application.process.binary"     : "ut",
+//     "application.process.user"       : "root",
+//     "application.process.host"       : "ut3",
+//     "application.process.machine_id" : "13123214124"
 // }
 //
-// and vice versa. Non-string values are implicitly converted to string by deflate.
+// and vice versa. The following rules apply:
+//  * Deflate converts any non-string values to string
+//
+//  * If a property has a value and sub-properties at the same time, as is the
+//    case with "application.process" in the above example, the property's value
+//    is stored in a second property with a @ prepended to its name.
+//
+//  * If a property does not have sub-properties, its value is stored directly
+//    under the property's name.
+//
+//  * deflate automatically handles @property values, reconstructing the
+//    original object of flat key-value pairs.
+//
+//  * The @ syntax should only be used if the property has a value and
+//    sub-properties at the same time.
+//
+//  * If the property name starts with @, it must only have a primitive value.
 
 
 export function deflate(props: Props, res: any = {}, prefix = ''): FlatProps {
     for (const [key, val] of Object.entries(props)) {
-        if (typeof val === 'object' && !Array.isArray(val) && val !== null)
+        if (typeof val === 'object' && !Array.isArray(val) && val !== null) {
             deflate(props[key] as Props, res, `${prefix}${key}.`);
-        else
-            res[`${prefix}${key}`] = `${val}`;
+        } else {
+            const k = key.startsWith('@') ? key.substring(1) : key;
+            res[`${prefix}${k}`] = `${val}`;
+        }
     }
     return res;
 }
 
 
 export function inflate(props: FlatProps): Props {
-    const res: any = {};
+    const res: Props = {};
     for (const [key, val] of Object.entries(props)) {
         const c = key.split('.');
-        let l = res;
+
+        let l: Record<string, any> = res;
         for (let i = 0; i < c.length - 1; i++) {
-            if (!(c[i] in l)) l[c[i]] = {};
+            if (c[i] in l) {
+                if (typeof l[c[i]] !== 'object') {
+                    l[`@${c[i]}`] = l[c[i]];
+                    delete l[c[i]];
+                }
+            }
+            if (typeof l[c[i]] === 'undefined') {
+                l[c[i]] = {};
+            }
             l = l[c[i]];
         }
 
-        try {
-            l[c[c.length - 1]] = val;
-        } catch (e) {
-            if (typeof res._weird === 'undefined') res._weird = {};
-            res._weird[key] = val;
-        }
+        l[c[c.length - 1]] = val;
     }
     return res;
 }
